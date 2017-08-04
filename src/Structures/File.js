7fe44e9f6aa974fcb46a37348c13ce860b75342f
Snazzah <suggesttosnazzy@gmail.com>
2017-08-04 11:50:33 -0500

    1.1.1: Fix files

--- src/Structures/File.js
+++ src/Structures/File.js
@@ -10,7 +10,7 @@ class File {
 
 	fetchUrl() {
 	    return new Promise((resolve, reject) => {
-			this.bot.getFileLink(this.id).then(url => {
+			this.bot.fancy.getFileLink(this.id).then(url => {
 				resolve(url);
 			}).catch(reject);
 		});
@@ -18,18 +18,14 @@ class File {
 
 	download(path) {
 	    return new Promise((resolve, reject) => {
-			this.bot.downloadFile(this.id, path).then(res => {
+			this.bot.fancy.downloadFile(this.id, path).then(res => {
 				resolve(res);
 			}).catch(reject);
 		});
 	}
 
 	fetch(path) {
-	    return new Promise((resolve, reject) => {
-			this.bot.getFile(this.id).then(res => {
-				resolve(new File(res));
-			}).catch(reject);
-		});
+	    return this.bot.fetchFile(this.id);
 	}
 }
 
